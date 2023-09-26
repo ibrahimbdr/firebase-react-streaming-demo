6dbaa302dfcad88a071c1e6ea84832abbd49e3c5
ibrahimbdr <ibrahimbdr@outlook.com>
2023-09-26 11:59:46 +0300

    update

--- src/videoRecorder.js
+++ src/videoRecorder.js
@@ -1,85 +1,191 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
-import RecordRTC from 'recordrtc';
-import { initializeApp } from "firebase/app";
-import { getStorage, ref, uploadString } from 'firebase/storage';
+import RecordRTC, { StereoAudioRecorder } from 'recordrtc';
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from 'firebase/storage';
+import { storage } from './firebaseConfig';
+import { FiCamera, FiStopCircle, FiMic, FiVideo } from 'react-icons/fi';
 
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCTbIxQmqNOkPVfwPbUc5GAvWOVW37PUyI",
-    authDomain: "video-stream-demo-c4c20.firebaseapp.com",
-    projectId: "video-stream-demo-c4c20",
-    storageBucket: "video-stream-demo-c4c20.appspot.com",
-    messagingSenderId: "250572019373",
-    appId: "1:250572019373:web:8191e6821a03ad25051824"
-};
+export default function VideoRecording() {
+  const webcamRef = useRef(null);
+  const [recordingStatus, setRecordingStatus] = useState('notRecording');
+  const [audioBlob, setAudioBlob] = useState(null);
+  const [micStream, setMicStream] = useState(null);
+  const [videoUrl, setVideoUrl] = useState(null);
+  const [activeTab, setActiveTab] = useState('recording');
+  const [isMicOn, setIsMicOn] = useState(false);
+  const [mediaRecorder, setMediaRecorder] = useState(null); 
+  const startMicrophone = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      setMicStream(stream);
+      setIsMicOn(true);
+    } catch (error) {
+      console.error('Error accessing microphone:', error);
+    }
+  };
 
-// Initialize Firebase
-initializeApp(firebaseConfig);
+  const stopMicrophone = () => {
+    if (micStream) {
+      micStream.getTracks().forEach((track) => track.stop());
+      setMicStream(null);
+      setIsMicOn(false);
+    }
+  };
 
-const VideoRecorder = () => {
-    const webcamRef = useRef(null);
-    const [recording, setRecording] = useState(false);
-    const [recordedVideoURL, setRecordedVideoURL] = useState('');
-    const [mediaRecorder, setMediaRecorder] = useState(null);
-
-    // Create a reference to the Firebase storage bucket
-    const storage = getStorage();
-    const storageRef = ref(storage, 'videos/' + Date.now() + '.webm');
-
-    const startRecording = async () => {
-        if (webcamRef.current) {
-            const stream = webcamRef.current.video.srcObject;
-            const recorder = new RecordRTC(stream, {
-                type: 'video',
-                mimeType: 'video/webm',
-            });
-
-            recorder.startRecording();
-
-            setMediaRecorder(recorder);
-            setRecording(true);
-        }
+  const handleStartRecording = async () => {
+    if (recordingStatus === 'uploaded') {
+      setRecordingStatus('notRecording');
+      setVideoUrl(null);
+      setAudioBlob(null);
+    }
+  
+    const recorder = new MediaRecorder(webcamRef.current.stream);
+    setMediaRecorder(recorder);
+    const audioRecorder = new RecordRTC(webcamRef.current.stream, {
+      type: 'video',
+      mimeType: 'video/webm',
+      recorderType: StereoAudioRecorder,
+      audio: micStream,
+    });
+  
+    const recordedChunks = [];
+  
+    recorder.ondataavailable = (e) => {
+      if (e.data.size > 0) {
+        recordedChunks.push(e.data);
+      }
     };
-
-    const stopRecording = async () => {
-        if (mediaRecorder) {
-            mediaRecorder.stopRecording(async () => {
-                const blob = mediaRecorder.getBlob();
-                const videoURL = URL.createObjectURL(blob);
-
-                setRecordedVideoURL(videoURL);
-
-                // Upload the recorded video to Firebase Storage
-                await uploadString(storageRef, blob, 'raw');
-
-                setRecording(false);
-            });
-        }
+  
+    recorder.onstop = async () => {
+      audioRecorder.stopRecording(() => {
+        const audioBlob = audioRecorder.getBlob();
+        setAudioBlob(audioBlob);
+        stopMicrophone();
+      });
+  
+      const videoBlob = new Blob(recordedChunks, {
+        type: 'video/webm',
+      });
+  
+      const videoFile = new File([videoBlob], 'video.webm', {
+        type: 'video/webm',
+      });
+  
+      const audioFile = new File([audioBlob], 'audio.wav', {
+        type: 'audio/wav',
+      });
+  
+      const videoRef = ref(storage, 'videos/video.webm');
+      const audioRef = ref(storage, 'audio/audio.wav');
+  
+      try {
+        console.log('Uploading video file...');
+        await uploadBytes(videoRef, videoFile);
+        console.log('Video file uploaded successfully.');
+  
+        console.log('Uploading audio file...');
+        await uploadBytes(audioRef, audioFile);
+        console.log('Audio file uploaded successfully.');
+  
+        const videoDownloadURL = await getDownloadURL(videoRef);
+        setVideoUrl(videoDownloadURL);
+  
+        setRecordingStatus('uploaded');
+        console.log('Recording uploaded successfully!');
+      } catch (error) {
+        console.error('Error uploading files:', error);
+      }
     };
+  
+    recorder.start();
+    audioRecorder.startRecording();
+    startMicrophone();
+    setRecordingStatus('recording');
+  };
+
+  const handleStopRecording = async () => {
+    setRecordingStatus('stopped');
+
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+    }
+  };
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
 
 
-    return (
-        <div className="video-recorder">
-            <Webcam
-                audio={false}
-                ref={webcamRef}
-                mirrored={true}
-                screenshotFormat="image/jpeg"
-                className="webcam"
-            />
-            <div className="controls">
-                {recording ? (
-                    <button onClick={stopRecording}>Stop Recording</button>
-                ) : (
-                    <button onClick={startRecording}>Start Recording</button>
-                )}
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-slate-400">
+      <div className="flex space-x-4">
+        <button
+          onClick={() => handleTabChange('recording')}
+          className={`flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mb-4 ${
+            activeTab === 'recording' ? 'bg-blue-700' : ''
+          }`}
+        >
+          <FiCamera className="mr-2" /> Record
+        </button>
+        <button
+          onClick={() => handleTabChange('video')}
+          className={`flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mb-4 ${
+            activeTab === 'video' ? 'bg-blue-700' : ''
+          }`}
+        >
+          <FiVideo className="mr-2" /> View Video
+        </button>
+      </div>
+
+      {activeTab === 'recording' && (
+        <>
+          <Webcam audio ref={webcamRef} className="border rounded mb-4" />
+          {recordingStatus === 'notRecording' && (
+            <button
+              onClick={handleStartRecording}
+              className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mb-4"
+            >
+              <FiCamera className="mr-2" /> Start Recording
+            </button>
+          )}
+          {recordingStatus === 'recording' && (
+            <button
+              onClick={handleStopRecording}
+              className="flex items-center justify-center bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mb-4"
+            >
+              <FiStopCircle className="mr-2" /> Stop Recording
+            </button>
+          )}
+          {recordingStatus === 'uploaded' && videoUrl && (
+            <div className="mb-4">
+              <p className="text-lg font-bold text-green-600">Recording uploaded successfully!</p>
             </div>
-            {recordedVideoURL && (
-                <video src={recordedVideoURL} controls className="recorded-video" />
-            )}
-        </div>
-    );
-};
+          )}
+          {micStream && isMicOn && (
+            <div className="flex items-center justify-center border rounded p-4">
+              <FiMic className="text-blue-500 mr-2" /> Microphone Enabled
+            </div>
+          )}
+        </>
+      )}
 
-export default VideoRecorder;
+      {activeTab === 'video' && (
+        <>
+          {recordingStatus === 'uploaded' && videoUrl ? (
+            <div className="mb-4">
+              <video controls src={videoUrl} className="border rounded" />
+            </div>
+          ) : (
+            <div className="flex items-center justify-center border rounded p-4">
+              <FiVideo className="text-blue-500 mr-2" /> No recorded video available
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
