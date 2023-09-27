53bcdd902ecb78b62d0dceb9f0448900630d6b57
ibrahimbdr <ibrahimbdr@outlook.com>
2023-09-27 09:46:16 +0300

    update

--- src/videoRecorder.js
+++ src/videoRecorder.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
 import RecordRTC, { StereoAudioRecorder } from 'recordrtc';
 import {
@@ -17,7 +17,94 @@ export default function VideoRecording() {
   const [videoUrl, setVideoUrl] = useState(null);
   const [activeTab, setActiveTab] = useState('recording');
   const [isMicOn, setIsMicOn] = useState(false);
-  const [mediaRecorder, setMediaRecorder] = useState(null); 
+  const [mediaRecorder, setMediaRecorder] = useState(null);
+  const [recordId, setRecordId] = useState(null);
+  const [counter, setCounter] = useState(0);
+  const [timerInterval, setTimerInterval] = useState(null);
+  const [currentMediaRecorder, setCurrentMediaRecorder] = useState(null);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+
+  useEffect(() => {
+    if (recordingStatus === 'recording') {
+      setTimerInterval(setInterval(() => {
+        setCounter(prevCounter => prevCounter + 1);
+      }, 1000));
+    } else {
+      clearInterval(timerInterval);
+      setCounter(0);
+    }
+  }, [recordingStatus]);
+
+  useEffect(() => {
+    if (recordingStatus === 'recording' && counter % 10 === 0) {
+      if (currentMediaRecorder) {
+        currentMediaRecorder.stop();
+        setCurrentMediaRecorder(null);
+      }
+
+      const recorder = new MediaRecorder(webcamRef.current.stream);
+      setCurrentMediaRecorder(recorder);
+
+      const audioRecorder = new RecordRTC(webcamRef.current.stream, {
+        type: 'video',
+        mimeType: 'video/webm',
+        recorderType: StereoAudioRecorder,
+        audio: micStream,
+      });
+
+      const newRecordedChunks = [...recordedChunks];
+
+      recorder.ondataavailable = (e) => {
+        if (e.data.size > 0) {
+          newRecordedChunks.push(e.data);
+          setRecordedChunks(newRecordedChunks);
+        }
+      };
+
+      recorder.onstop = async () => {
+        audioRecorder.stopRecording(() => {
+          const audioBlob = audioRecorder.getBlob();
+          setAudioBlob(audioBlob);
+          stopMicrophone();
+        });
+
+        const videoBlob = new Blob(recordedChunks, {
+            type: 'video/webm',
+          });
+    
+          const videoFile = new File([videoBlob], `video_${counter}_${new Date().toISOString()}.webm`, {
+            type: 'video/webm',
+          });
+    
+          const audioFile = new File([audioBlob], `audio_${counter}_${new Date().toISOString()}.wav`, {
+            type: 'audio/wav',
+          });
+    
+          const videoRef = ref(storage, `videos/video_${counter}_${new Date().toISOString()}.webm`);
+          const audioRef = ref(storage, `audio/audio_${counter}_${new Date().toISOString()}.wav`);
+    
+          try {
+            console.log('Uploading video file...');
+            await uploadBytes(videoRef, videoFile);
+            console.log('Video file uploaded successfully.');
+    
+            console.log('Uploading audio file...');
+            await uploadBytes(audioRef, audioFile);
+            console.log('Audio file uploaded successfully.');
+    
+            const videoDownloadURL = await getDownloadURL(videoRef);
+            setVideoUrl(videoDownloadURL);
+    
+          } catch (error) {
+            console.error('Error uploading files:', error);
+          }
+      };
+
+      recorder.start();
+      audioRecorder.startRecording();
+    }
+  }, [counter, recordingStatus]);
+
   const startMicrophone = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -42,7 +129,7 @@ export default function VideoRecording() {
       setVideoUrl(null);
       setAudioBlob(null);
     }
-  
+
     const recorder = new MediaRecorder(webcamRef.current.stream);
     setMediaRecorder(recorder);
     const audioRecorder = new RecordRTC(webcamRef.current.stream, {
@@ -51,56 +138,62 @@ export default function VideoRecording() {
       recorderType: StereoAudioRecorder,
       audio: micStream,
     });
-  
-    const recordedChunks = [];
-  
+
+    // const recordedChunks = [];
+
     recorder.ondataavailable = (e) => {
       if (e.data.size > 0) {
-        recordedChunks.push(e.data);
+        setRecordedChunks(prev => [...prev, e.data])
+        // recordedChunks.push(e.data);
+        console.log(recordedChunks);
       }
     };
-  
+
     recorder.onstop = async () => {
       audioRecorder.stopRecording(() => {
         const audioBlob = audioRecorder.getBlob();
         setAudioBlob(audioBlob);
         stopMicrophone();
       });
-  
+
       const videoBlob = new Blob(recordedChunks, {
         type: 'video/webm',
       });
-  
-      const videoFile = new File([videoBlob], 'video.webm', {
+
+      const videoFile = new File([videoBlob], `video_${counter}_${new Date().toISOString()}.webm`, {
         type: 'video/webm',
       });
-  
-      const audioFile = new File([audioBlob], 'audio.wav', {
+
+      const audioFile = new File([audioBlob], `audio_${counter}_${new Date().toISOString()}.wav`, {
         type: 'audio/wav',
       });
-  
-      const videoRef = ref(storage, 'videos/video.webm');
-      const audioRef = ref(storage, 'audio/audio.wav');
-  
+
+      const videoRef = ref(storage, `videos/video_${counter}_${new Date().toISOString()}.webm`);
+      const audioRef = ref(storage, `audio/audio_${counter}_${new Date().toISOString()}.wav`);
+
       try {
         console.log('Uploading video file...');
         await uploadBytes(videoRef, videoFile);
         console.log('Video file uploaded successfully.');
-  
+
         console.log('Uploading audio file...');
         await uploadBytes(audioRef, audioFile);
         console.log('Audio file uploaded successfully.');
-  
+
         const videoDownloadURL = await getDownloadURL(videoRef);
         setVideoUrl(videoDownloadURL);
-  
+
         setRecordingStatus('uploaded');
         console.log('Recording uploaded successfully!');
       } catch (error) {
         console.error('Error uploading files:', error);
       }
     };
-  
+
+    // Generate a unique recordId
+    const newRecordId = Date.now();
+    setRecordId(newRecordId);
+
     recorder.start();
     audioRecorder.startRecording();
     startMicrophone();
@@ -119,6 +212,11 @@ export default function VideoRecording() {
     setActiveTab(tab);
   };
 
+  const formatTimer = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-slate-400">
@@ -153,12 +251,17 @@ export default function VideoRecording() {
             </button>
           )}
           {recordingStatus === 'recording' && (
-            <button
-              onClick={handleStopRecording}
-              className="flex items-center justify-center bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mb-4"
-            >
-              <FiStopCircle className="mr-2" /> Stop Recording
-            </button>
+            <div className="mb-4">
+              <p className="text-2xl text-center font-semibold">
+                {formatTimer(counter)}
+              </p>
+              <button
+                onClick={handleStopRecording}
+                className="flex items-center justify-center bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mt-4"
+              >
+                <FiStopCircle className="mr-2" /> Stop Recording
+              </button>
+            </div>
           )}
           {recordingStatus === 'uploaded' && videoUrl && (
             <div className="mb-4">
